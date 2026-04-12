8b932cfaf0050846ba2f397fe2fceedfd4231e18
eveyang67 <eveyang67@163.com>
2026-04-12 15:13:13 +0800

    Revert site to f496a16 baseline

--- js/random-walk.js
+++ js/random-walk.js
@@ -24,7 +24,7 @@
         return photoPath.replace('images/wishes/', 'images/wishes/thumbs/');
     }
 
-    function warmRandomWishImages(limit = wishesData.length) {
+    function warmRandomWishImages(limit = 8) {
         const shuffled = wishesData
             .map((item, index) => ({ item, sortKey: ((index * 17) + 11) % wishesData.length }))
             .sort((left, right) => left.sortKey - right.sortKey)
